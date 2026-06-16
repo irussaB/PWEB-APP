7eec2f3124c7691e81385fd7e70f6d3bf6a272bf
irussaB <barussidemoraespedrohenrique@gmail.com>
2026-06-16 09:03:36 -0300

    Alteração da interface de produtos

--- produto/index.js
+++ produto/index.js
@@ -1,18 +1,20 @@
 const apiUrl = 'http://localhost/PWEB-API/api/produtos/';
 
-document.getElementById('frmProdutos').addEventListener('submit', function (event) {
+document.getElementById('frmProduto').addEventListener('submit', function (event) {
     event.preventDefault();
     let nome = document.getElementById('nome').value;
+    let valorvenda = document.getElementById('valorvenda').value;
+    let categoria = document.getElementById('categoria').value;
     let operacao = 'i';
     fetch(`${apiUrl}index.php?json=${JSON.stringify({ op:operacao,nome:nome,vlvenda:valorvenda,cat:categoria })}`)
         .then(response => response.text())
         .then(() => {
-            alert('Categoria cadastrada!');
+            alert('Produto cadastrado!');
             window.location.reload();
         });
 });
 
-function carregarcategorias() {
+function carregarprodutos() {
     let operacao = 's';
     fetch(`${apiUrl}index.php?json=${JSON.stringify({ op:operacao })}`)
         .then(response => response.json())
@@ -23,9 +25,10 @@ function carregarcategorias() {
                 gridCategoria.innerHTML += `
             <div class="row mt-2">
                 <div class="col-sm-3">${ds.nome}</div>
-                <div class="col-sm-2">${ds.ativo ? 'Ativo' : 'Inativo'}</div>
+                <div class="col-sm-2">${ds.vlvenda}</div>
+                <div class="col-sm-1">${ds.cat}</div>
                 <div class="col-sm-4">
-                    <button class="btn btn-warning" onclick="editarcategoria(${ds.id}, '${ds.nome}', '${ds.ativo}')">Editar</button>
+                    <button class="btn btn-warning" onclick="editarcategoria(${ds.id}, '${ds.nome}', '${ds.vlvenda}', '${ds.cat}')">Editar</button>
                     <button class="btn btn-danger" onclick="deletarcategoria(${ds.id}, '${ds.nome}')">Excluir</button>
                 </div>
             </div>`;
@@ -33,20 +36,24 @@ function carregarcategorias() {
         });
 }
 
-function editarcategoria(id, nome, ativo) {
+function editarcategoria(id, nome, vlvenda, cat) {
     document.getElementById('nome').value = nome;
-    const btn = document.querySelector('#frmCategoria button[type="submit"]');
+    document.getElementById('valorvenda').value = vlvenda;
+    document.getElementById('categoria').value = cat;
+    const btn = document.querySelector('#frmProduto button[type="submit"]');
     btn.textContent = 'ATUALIZAR';
     btn.classList.remove('btn-primary');
     btn.classList.add('btn-success');
     btn.onclick = function (event) {
         event.preventDefault();
         let unome = document.getElementById('nome').value;
+        let uvalorvenda = document.getElementById('valorvenda').value;
+        let ucategoria = document.getElementById('categoria').value;
         let operacao = 'u';
-        fetch(`${apiUrl}index.php?json=${JSON.stringify({ op:operacao ,nome: unome, ativo:ativo, id:id })}`)
+        fetch(`${apiUrl}index.php?json=${JSON.stringify({ op:operacao,nome:unome,vlvenda:uvalorvenda,cat:ucategoria,id:id })}`)
             .then(response => response.text())
             .then(() => {
-                alert('Categoria alterada!');
+                alert('Produto alterado!');
                 window.location.reload();
             });
     }
@@ -54,8 +61,8 @@ function editarcategoria(id, nome, ativo) {
 
 function deletarcategoria(id, nome) {
     document.getElementById('nome').value = 'Deseja realmente excluir ' + nome;
-    const btn = document.querySelector('#frmCategoria button[type="submit"]');
-    const txt = document.querySelector('#frmCategoria input[type="text"]');
+    const btn = document.querySelector('#frmProduto button[type="submit"]');
+    const txt = document.querySelector('#frmProduto input[type="text"]');
     btn.textContent = 'EXCLUIR';
     btn.classList.remove('btn-primary');
     btn.classList.add('btn-danger');
@@ -66,9 +73,9 @@ function deletarcategoria(id, nome) {
         fetch(`${apiUrl}index.php?json=${JSON.stringify({ op:operacao,id:id })}`)
             .then(response => response.text())
             .then(() => {
-                alert('Categoria Excluida!');
+                alert('Produto Excluida!');
                 window.location.reload();
             });
     }
 }
-window.onload = carregarcategorias;
+window.onload = carregarprodutos;
